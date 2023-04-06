fd4f043d3bb782dfb8e0320513a29d67afd5f2b7
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-06 17:46:51 -0400

    commit tree fa1345ae14f50caca9dc9e671e5bc3256d855ed7

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+ ("Mr. Chocolate", 7, "M", "Brown", "Happy", 1),
+ ("Rowdy", 3, "M", "Black", "Playful", 1),
+ ("Tabitha", 35, "F", "Light Brown", "Sweet", 0),
+ ("Sergeant Brown", 31, "M", "light brown", "Bossy", 1),
+ ("Melissa", 21, "F", "Black", "Happy", 1),
+ ("Grinch", 15, "M", "Brown", "Grouchy", 1),
+ ("Wendy",10, "F", "Panda", "Playful", 1),
+ (NULL, 1, "M", "light brown", "Sweet", 1)
+ 
